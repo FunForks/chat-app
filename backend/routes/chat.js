d7504030c8bfa214fad72bc3b46b98b34b4e0ded
James Newton <blackslate@lexogram.com>
2023-01-22 10:37:51 +0300

    Add /poll route to broadcast messages
    In backend/routes/chat.js
    * Add `pollResponses` array
    * Add call to `broadcast(reply)` to the function for `/`
    * Add a function for the route `/poll` which saves responses in the `pollResponses` array
    * Add a broadcast(reply) function that uses the responses stored in `pollResponses` to send a reply to all connected clients
    
    * Update CHANGELOG.md
    * Update README.md
    * Provide new TASK.md

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,15 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const pollResponses = []
+
+
 /* Echo message from POST request */
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
   const { message } = req.body
   const timeStamp = new Date().toLocaleTimeString()
   const reply = {
     message,
     timeStamp
   }
-  res.send(reply);
+  res.send(reply) // reply to calling client
+
+  broadcast(reply) // send message to all connected clients
 });
+
+
+/* Store responses from /poll POST request for later use */
+router.post('/poll', function(req, res) {
+  pollResponses.push(res)
+});
+
+
+// Use pollResponse to broadcast messages from one client to
+// all connected clients
+function broadcast(reply) {
+  let response
+  while (response = pollResponses.shift()) {
+    response.send(reply)
+  }
+}
+
 
 module.exports = router;
